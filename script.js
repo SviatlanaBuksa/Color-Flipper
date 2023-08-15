61433bb87c991672eed27fe834043bb139cee703
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-15 15:30:52 +0200

    h1 color value changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let colors = ["black", "green", "yellow", "#CA8A4B", "rgba(133,122,200)"];
+let maxNumber = colors.length;
 
 let changeColorBtn = document.querySelector("#change-color-btn");
 let colorValue = document.querySelector("#color");
@@ -6,12 +7,18 @@ let colorValue = document.querySelector("#color");
 changeColorBtn.addEventListener("click", changeBgColor);
 
 function changeBgColor() {
-  document.body.style.backgroundColor = `${getRandomColor(5)}`;
+  document.body.style.backgroundColor = `${getRandomColor(maxNumber)}`;
 }
 
 function getRandomColor(max) {
   let randomNumber = Math.floor(Math.random() * max);
   let color = colors[randomNumber];
 
+  changeColorValue(color);
+
   return color;
+}
+
+function changeColorValue(color) {
+  colorValue.innerHTML = `${color}`;
 }
