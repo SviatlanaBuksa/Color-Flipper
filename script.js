9d1892031398063c932634bfa91e1060eef63e7e
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-18 15:16:11 +0200

    Color flipper version attached to link clicks

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 let colors = ["black", "green", "yellow", "#CA8A4B", "rgba(133,122,200)"];
-let maxNumber = colors.length;
 
 let changeColorBtn = document.querySelector("#change-color-btn");
 let colorValue = document.querySelector("#color");
 
+let simpleLink = document.querySelector("#simple");
+let hexLink = document.querySelector("#hex");
+
+hexLink.addEventListener("click", selectHexVersion);
+simpleLink.addEventListener("click", selectSimpleVersion);
 changeColorBtn.addEventListener("click", changeBgColor);
 
 function changeBgColor() {
-  document.body.style.backgroundColor = `${getRandomHex()}`;
+  if (hexLink.className == "active") {
+    document.body.style.backgroundColor = `${getRandomHex()}`;
+  } else {
+    document.body.style.backgroundColor = `${getRandomColor()}`;
+  }
 }
 
-/*function getRandomColor(max) {
-  let randomNumber = Math.floor(Math.random() * max);
+function selectHexVersion() {
+  hexLink.classList.add("active");
+  simpleLink.classList.remove("active");
+}
+
+function selectSimpleVersion() {
+  hexLink.classList.remove("active");
+  simpleLink.classList.add("active");
+}
+
+function getRandomColor() {
+  let randomNumber = getRandomNumber(colors.length);
   let color = colors[randomNumber];
 
   changeColorValue(color);
@@ -21,14 +39,25 @@ function changeBgColor() {
 
 function changeColorValue(color) {
   colorValue.innerHTML = `${color}`;
-}  */
+}
+
+function getRandomNumber(max) {
+  let randomNumber = Math.floor(Math.random() * max);
+  return randomNumber;
+}
 
 function getRandomHex() {
   let letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
   let hexColor = "#";
 
   for (let i = 0; i < 6; i++) {
-    hexColor += letters[Math.floor(Math.random() * 16)];
+    let maxNumber = letters.length;
+    let randomNumber = getRandomNumber(maxNumber);
+
+    hexColor += letters[randomNumber];
   }
+
+  changeColorValue(hexColor);
+
   return hexColor;
 }
